6725abf751ccd2d55655438101665a7d9087930e
tenkyuu <hello@tenkyuu.dev>
2024-10-27 15:10:55 +0800

    ill just save it.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// navigation
+const mobile_menu = document.querySelector(".mobile-menu");
+const nav_menu = document.querySelector(".nav-menu");
+
+mobile_menu.addEventListener("click", () => {
+  mobile_menu.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  mobile_menu.classList.remove("active"); 
+  nav_menu.classList.remove("active");
+}))
+
+function nullLink() {
+  alert("NO LINK FOR THIS PROJECT YET :D");
+}
+
+// navigation - page indicator
+const active_page = window.location.pathname;
+console.log(active_page);
+
+const nav_links = document.querySelectorAll(".nav-link").forEach(link => {
+  if(link.href.includes(`${active_page}`)) {
+    link.classList.add("active-page");
+  }
+})
